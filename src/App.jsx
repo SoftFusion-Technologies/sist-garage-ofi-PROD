78765c9b24e48365cd7ee890cf7882188fc34afb
Benjamin <goosta19802@gmail.com>
2025-06-21 19:09:30 -0300

    Se agrega login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
-import './App.css'
+/*
+ * Programador: Benjamin Orellana
+ * Fecha Creación: 26 / 05 / 2025
+ * Versión: 1.0
+ *
+ * Descripción:
+ *  Este archivo (App.jsx) es el componente principal de la aplicación.
+ *  Contiene la configuración de enrutamiento, carga de componentes asíncrona,
+ *  y la lógica para mostrar un componente de carga durante la carga inicial.
+ *  Además, incluye la estructura principal de la aplicación, como la barra de navegación,
+ *  el pie de página y las diferentes rutas para las páginas de la aplicación.
+ *
+ * Tema: Configuración de la Aplicación Principal
+ * Capa: Frontend
+ * Contacto: benjamin.orellanaof@gmail.com || 3863531891
+ */
 
-function App() {
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes as Rutas,
+  Route as Ruta
+} from 'react-router-dom'; // IMPORTAMOS useLocation PARA OCULTAR COMPONENTES
+
+import { AuthProvider } from './AuthContext';
+import ProtectedRoute from './ProtectedRoute';
+import useLayoutVisibility from './Hooks/useLayoutVisibility';
+
+
+// LOGIN
+import LoginForm from './Components/login/LoginForm';
+function AppContent() {
+  const { hideLayoutFooter, hideLayoutNav } = useLayoutVisibility();
 
   return (
     <>
+      {!hideLayoutNav && <NavBar />}
+      <Rutas>
+        {/* <Ruta path="/" element={<Home />} /> */}
+        {/* componentes del staff y login INICIO */}
+        <Ruta path="/login" element={<LoginForm />} />
+        {/* componentes del staff y login FINAL */}
+        {/* <Ruta path="/*" element={<NotFound />} /> */}
+      </Rutas>
+      {/* {!hideLayoutFooter && <Footer />} */}
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
